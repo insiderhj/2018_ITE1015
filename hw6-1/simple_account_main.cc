28368281813cf13e6c065456375385e1a0e65046
2018008259 <2018008259@hanyang.ac.kr>
2018-10-16 19:59:08 +0900

    hw6-1

--- hw6-1/simple_account_main.cc
+++ hw6-1/simple_account_main.cc
@@ -16,42 +16,43 @@ int main()
 		if (job == 'N') {
 			if (editor.get_count() < 9) {
 				editor.make_acc();
-				cout << "Account for user " << editor.get_count() << " registered" << endl;
-				cout << "Balance of user " << editor.get_count() << ": " << editor.check(editor.get_count()) << endl;
-			}
+				myid = editor.get_count();
+				cout << "Account for user " << myid << " registered" << endl;
+			} else continue;
 		}
 		else if (job == 'D') {
 			cin >> myid >> money;
 			if (editor.get_count() >= myid) {
-				if (editor.deposit(myid, money)) cout << "Success: Deposit to user " << myid << " " << money << endl;
-				else cout << "Failure: Deposit to user " << myid << " " << money << endl;
-				cout << "Balance of user " << myid << ": " << editor.check(myid) << endl;
+				if (editor.deposit(myid, money)) cout << "Success: ";
+				else cout << "Failure: ";
+				cout << "Deposit to user " << myid << " " << money << endl;
 			} else cout << "Account does not exist" << endl;
 		}
 
 		else if (job == 'W') {
 			cin >> myid >> money;
 			if (editor.get_count() >= myid) {
-				if (editor.withdraw(myid, money)) cout << "Success: Withdraw from user " << myid << " " << money << endl;
-				else cout << "Failure: Deposit to user " << myid << " " << money << endl;
-				cout << "Balance of user " << myid << ": " << editor.check(myid) << endl;
+				if (editor.withdraw(myid, money)) cout << "Success: ";
+				else cout << "Failure: ";
+				cout << "Deposit to user " << myid << " " << money << endl;
 			} else cout << "Account does not exist" << endl;
 		}
 		else if (job == 'T') {
 			cin >> myid >> toid >> money;
 			if (editor.get_count() >= myid && editor.get_count() >= toid) {
 				if (editor.withdraw(myid, money)) {
-					if (editor.deposit(toid, money)) cout << "Success: Transfer from user " << myid << " to user " << toid << " " << money << endl;
+					if (editor.deposit(toid, money)) cout << "Success: ";
 					else {
 						editor.deposit(myid, money);
-						cout << "Failure: Transfer from user " << myid << " to user " << toid << " " << money << endl;
+						cout << "Failure: ";
 					}
-				} else cout << "Failure: Transfer from user " << myid << " to user " << toid << " " << money << endl;
-				cout << "Balance of user " << myid << ": " << editor.check(myid) << endl;
+				} else cout << "Failure: ";
+				cout << "Transfer from user " << myid << " to user " << toid << " " << money << endl;
 				cout << "Balance of user " << toid << ": " << editor.check(toid) << endl;
 			} else cout << "Account does not exist" << endl;
 		}
 		else return 0;
+		cout << "Balance of user " << myid << ": " << editor.check(myid) << endl;
 	}
 	return 0;
 }
