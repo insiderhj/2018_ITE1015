5cd51bc6d61531768b85a668df9f49220f845262
2018008259 <2018008259@hanyang.ac.kr>
2018-10-18 22:07:18 +0900

    hw6-3

---
+++ hw6-3/minimal_fighter.h
@@ -0,0 +1,27 @@
+#ifndef _MINIMAL_FIGHTER_
+#define _MINIMAL_FIGHTER_
+
+enum FighterStatus {Invalid, Alive, Dead};
+
+class MinimalFighter {
+private:
+	int mHp;
+	int mPower;
+	FighterStatus mStatus;
+public:
+	MinimalFighter();
+	MinimalFighter(int, int);
+	~MinimalFighter();
+	int hp() const;
+	int power() const;
+	FighterStatus status() const;
+	void setHp(int);
+	void hit(MinimalFighter*);
+	void attack(MinimalFighter*);
+	void fight(MinimalFighter*);
+};
+
+std::string* split(std::string&, const std::string&);
+bool check_number(std::string);
+
+#endif
---
+++ hw6-3/minimal_fighter_main.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "minimal_fighter.h"
+
+using namespace std;
+
+int main() {
+	string argument;
+	string* todo;
+	MinimalFighter* fighter;
+	MinimalFighter* enemy;
+
+	while (true) {
+		getline(cin, argument);
+		todo = split(argument, " ");
+
+		if (todo[4] == "") return -1;
+		if (todo[5] != "") return -1;
+		for (int i = 0; i < 5; i++) {
+			if (i == 2) {
+				if (!(todo[i] == "H" || todo[i] == "A" || todo[i] == "F")) return -1;
+			} else if (!check_number(todo[i])) return -1;
+		}
+	
+		fighter = new MinimalFighter(atoi(todo[0].c_str()), atoi(todo[1].c_str()));
+		enemy = new MinimalFighter(atoi(todo[3].c_str()), atoi(todo[4].c_str()));
+
+		if (todo[2] == "H") fighter->hit(enemy);
+		else if (todo[2] == "A") fighter->attack(enemy);
+		else if (todo[2] == "F") fighter->fight(enemy);
+
+		if (fighter->status() == Alive) cout << "H" << fighter->hp() << ", P" << fighter->power() << " / ";
+		else cout <<"DEAD / ";
+
+		if (enemy->status() == Alive) cout << "H" << enemy->hp() << ", P" << enemy->power() << endl;
+		else cout << "DEAD" << endl;
+	}
+
+	return 0;
+}
